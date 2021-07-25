7fdfe820fb93b96db986d6f1a42069b6fed5d73d
felixyunianto <felixyunianto@gmail.com>
2021-07-26 04:07:50 +0700

    5. Create notif bencana

--- app/Http/Controllers/BencanaController.php
+++ app/Http/Controllers/BencanaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Bencana;
+use App\User;
 use Cloudinary;
 use Carbon\Carbon;
 use Validator;
@@ -53,6 +54,12 @@ class BencanaController extends Controller
             'public_id' => $request->foto ? $publicId : null,
         ]);
 
+        $deviceToken = User::whereNotNull('device_token')->pluck('device_token')->all();
+
+        $this->sendNotification("Pemberitahuan", 
+            "Terjadi bencana " . $bencana->nama." pada tanggal ". Carbon::parse($bencana->tanggal)->format("d M Y"),
+        $deviceToken);
+
         return response()->json([
             'message' => 'Berhasil menambahkan data bencana',
             'status' => 200,
@@ -99,5 +106,53 @@ class BencanaController extends Controller
             'data' => $bencana
         ],200);
 
+    }
+
+    public function sendNotification($title, $body, $token){
+        $data = [
+            'title' => $title,
+            'body' => $body,
+        ];
+
+        $device_token = [];
+        
+        foreach($token as $t){
+            // dd($t);
+            $device_token[] = $t;
+        }
+
+        // dd($device_token);
+
+        $payload = [
+            'registration_ids' => $device_token,
+            'notification' => $data
+        ];
+
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://fcm.googleapis.com/fcm/send",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_HTTPHEADER => array(
+                "Content-type: application/json",
+                "Authorization: key=".env('FIREBASE_SERVER_KEY')
+            ),
+        ));
+
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($payload));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+        
+        return response()->json([
+            'message' => 'Berhasil mengirim notif',
+            'status' => 200,
+            'data' => json_encode($response)
+        ], 200);
     }
 }
--- app/Http/Controllers/PenerimaanLogistikController.php
+++ app/Http/Controllers/PenerimaanLogistikController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\PenerimaanLogistik;
 use App\LogistikKeluar;
 use App\Logistik;
+use App\User;
 
 class PenerimaanLogistikController extends Controller
 {
@@ -29,6 +30,13 @@ class PenerimaanLogistikController extends Controller
         $keluar = LogistikKeluar::findOrFail($id);
         $produk = Logistik::where('id', $keluar->id_produk)->first();
         $produk_penerimaan = Logistik::where('nama_produk', $produk->nama_produk)->where('id_posko', $keluar->penerima_id)->where('satuan', $keluar->satuan)->first();
+
+
+        $deviceToken = User::whereNotNull('device_token')->where('level','Admin')->pluck('device_token')->all();
+
+        $this->sendNotification("Pemberitahuan", 
+            $keluar->produk->nama_produk." telah diterima oleh ".$keluar->posko_penerima->nama, 
+        $deviceToken);
 
         if($produk_penerimaan == null){
             $produk_penerimaan = Logistik::create([
@@ -63,5 +71,53 @@ class PenerimaanLogistikController extends Controller
             'data' => 'BERHASIL'
         ], 200);
         
+    }
+
+    public function sendNotification($title, $body, $token){
+        $data = [
+            'title' => $title,
+            'body' => $body,
+        ];
+
+        $device_token = [];
+        
+        foreach($token as $t){
+            // dd($t);
+            $device_token[] = $t;
+        }
+
+        // dd($device_token);
+
+        $payload = [
+            'registration_ids' => $device_token,
+            'notification' => $data
+        ];
+
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://fcm.googleapis.com/fcm/send",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_HTTPHEADER => array(
+                "Content-type: application/json",
+                "Authorization: key=".env('FIREBASE_SERVER_KEY')
+            ),
+        ));
+
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($payload));
+
+        $response = curl_exec($curl);
+        curl_close($curl);
+        
+        return response()->json([
+            'message' => 'Berhasil mengirim notif',
+            'status' => 200,
+            'data' => json_encode($response)
+        ], 200);
     }
 }
